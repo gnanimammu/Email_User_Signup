a193d50be5e459695154155cd1017c6ef84a0546
Mamatha <p.mamatha237@gmail.com>
2024-06-30 08:48:59 +0000

    Added my project

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,26 @@
+class HomeController < ApplicationController
+	def index
+
+	end
+
+	def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user.present? && @user.authenticate(params[:password])
+      # user[:user_id] = user.id
+      @user
+      redirect_to root_path, notice: "Successfully logged in"
+    else
+      flash[:alert] = 'Invalid email or password'
+      render :new
+    end
+
+   def destroy
+    	@user = User.find_by(email: params[:email])
+    	@user = nil
+  		redirect_to root_path, notice: "Logged out successfully."
+	end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  root to: "home#index"
+
+  get "sign_up", to: "user#new" 
+  post "sign_up", to: "user#create"
+  get "log_in", to: "home#new"
+  post "log_in", to: "home#create"
+  # delete "logout", to: "home#destroy"
+  get "logout", to: "home#destroy"
+  get "home/create"
+  get "home/destroy"
+end
